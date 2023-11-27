20c2925adc06f35a30e1363acd9af914bfb4a550
Rossana Chi <amyeluzr@gmail.com>
2023-11-27 15:45:01 -0600

    Fin de sección 9

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -10,5 +10,13 @@ export class SearchBoxComponent {
 
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue: EventEmitter<string> = new EventEmitter();
+
+  searchTerm( term : string ) {
+    if(!(term.length > 0)) return;
+    this.onValue.emit( term );
+  }
 
 }
